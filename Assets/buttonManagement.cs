29b654e99527a46b1c82152e6258fbf3c2477669
Samuelp74 <Samuel.Pochat@etu.univ-savoie.fr>
2022-06-27 16:20:11 +0200

    Le menu
    Ajout du menu

---
+++ Assets/buttonManagement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class buttonManagement : MonoBehaviour
+{
+    public GameObject menuAmelioration;
+    public GameObject menuPrincipal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuAmelioration.SetActive(false);
+    }
+
+    public void Leave()
+    {
+        Application.Quit();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Jeu");
+    }
+    
+    public void Upgrade()
+    {
+        menuPrincipal.SetActive(false);
+        menuAmelioration.SetActive(true);
+    }
+    
+}
---
+++ Assets/volume.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class volume : MonoBehaviour
+{
+    [SerializeField] Slider sliderVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+        }
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+        AudioListener.volume = sliderVolume.value;
+        Save();
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", sliderVolume.value);
+    }
+
+    private void Load()
+    {
+        sliderVolume.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+}
